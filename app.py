e622dfc9b0167bcd68349b5a878f889cb695e551
RoHaNaLpHaCoDeR <rohanaroraaashi@gmail.com>
2025-02-16 22:29:04 +0530

    .

--- app.py
+++ app.py
@@ -10,11 +10,6 @@ def index():
 def redirect_to_link():
     return redirect('https://t.me/LinkInBioEnjoy')
 
-if __name__ == '__main__':
-    app.run(debug=True)
-    
-# For Vercel
-def handler(event, context):
-    from werkzeug.middleware.proxy_fix import ProxyFix
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
-    return app(event, context)
+if __name__ == "__main__":
+    from waitress import serve  # Optional for production
+    serve(app, host="0.0.0.0", port=8000)
